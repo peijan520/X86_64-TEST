7d9b983e5c75786dfc67aa1ca5ab39c712762b4b
YiuKi520 <44165796+YiuKi520@users.noreply.github.com>
2022-03-20 11:22:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+
 # Add a feed source
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
