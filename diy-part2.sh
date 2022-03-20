ae6f4c10feaf72b03bb4b07882bbec3410fbe3a7
YiuKi520 <44165796+YiuKi520@users.noreply.github.com>
2022-03-20 11:22:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+
 
 
 git clone https://github.com/linkease/istore.git package/istore
