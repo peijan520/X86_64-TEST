97ea24a94a42fc280567297e0fc02507be1e277d
YiuKi520 <44165796+YiuKi520@users.noreply.github.com>
2022-03-24 16:36:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 # Add a feed source
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/miaoerwu/helloworld' >>feeds.conf.default
+echo 'src-git https://github.com/miaoerwu/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
